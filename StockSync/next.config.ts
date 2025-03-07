2c6da26c45e90dff6f1906357285373fb4145ce1
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2025-03-07 22:06:15 +0530

    remove

--- StockSync/next.config.ts
+++ StockSync/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_VELT_API_KEY: process.env.NEXT_PUBLIC_VELT_API_KEY,
-  },
 };
 
 export default nextConfig;
