71d837fcc3f350837a80202a9d7d3ce98731e0f5
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2025-04-01 14:30:18 +0530

    change the user detail

--- spreadsheet/components/velt/VeltInitializeDocument.tsx
+++ spreadsheet/components/velt/VeltInitializeDocument.tsx
@@ -3,8 +3,8 @@ import { useSetDocument } from "@veltdev/react";
 // [VELT] Initialize the document with a unique identifier and metadata.
 
 export default function VeltInitializeDocument() {
-  useSetDocument("space-missions-spreadsheet-1", {
-    documentName: "space-missions-spreadsheet-1",
+  useSetDocument("space-missions-spreadsheet-org", {
+    documentName: "space-missions-spreadsheet-org",
   });
   return null;
 }
