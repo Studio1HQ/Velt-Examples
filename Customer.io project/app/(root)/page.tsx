29b07cbbb3c2e2e27ed6876308f535ff0b99c05e
YashChopra2511 <chopray567@gmail.com>
2025-07-29 20:47:39 +0530

    update for the  fixes

--- Customer.io project/app/(root)/page.tsx
+++ Customer.io project/app/(root)/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 
-import { useState } from 'react';
-import { EmailComposer } from '@/components/EmailComposer';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from "react";
+import { EmailComposer } from "@/components/EmailComposer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import useTheme, { ThemeProvider } from "@/components/useTheme";
+import { VeltComments } from "@veltdev/react";
 
 const queryClient = new QueryClient();
 
 export default function Home() {
+    const theme=useTheme()
+
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen bg-gray-50">
-        <EmailComposer />
-      </div>
+   
+        <div className="min-h-screen bg-gray-50 dark:bg-[#25293c]">
+          <EmailComposer />
+        </div>
+        <VeltComments
+          textMode={false}
+          shadowDom={false}
+          textCommentToolShadowDom={false}
+          darkMode={theme.theme === "dark"}
+        />
     </QueryClientProvider>
   );
 }
